a0b9876b4303320af2c49cf85c9200fc9074ad42
sam collet <origamillamaa@gmail.com>
2018-11-06 14:03:33 -0800

    comments

--- srcs/mini_main.c
+++ srcs/mini_main.c
@@ -6,6 +6,10 @@ char 	*mini_prompt(const char *prompt)
 	char	*input;
 
 	putstr(prompt);
+	/*
+	** In this line we're going to ingest the entire
+	** string of bytes provided on stdin.
+	*/
 	while (!(ret = mini_getline(STDIN_FILENO, &input)))
 		;
 	if (ret < 0)
@@ -17,10 +21,26 @@ int		mini_shell(t_env *env)
 {
 	char *input;
 
+	/*
+	** In the case where a user does not want to leave the shell, and the shell has
+	** not erred, it will run forever. On initialization we set the status of our
+	** environment to running as an on/off switch. At any time during the program,
+	** if we set this boolean to the off state, then we clean our memory and exit the
+	** program
+	*/
 	while (env->status.running)
 	{
+		/*
+		** every "cycle" of the shell we need to capture the user's input in a string
+		** this determines the execution of subprocesses in the shell
+		*/
 		if ((input = mini_prompt(PROMPT)))
 		{
+			/*
+			** to reduce code complexity we can employ a dispatch table. This is
+			** an iterable mapping of strings to function pointers and helps us
+			** standardize the way we handle program flow
+			*/
 			if (mini_dispatch(env, input) == FAILURE)
 				return (mini_error(env, __func__, NULL, "dispatch failed"));
 		}
@@ -34,9 +54,16 @@ int		main(void)
 {
 	t_env *env;
 
+	/*
+	** Always initialize your environment and check for errors on the return
+	*/
 	if (mini_init(&env))
 	{
 		putendl(WELCOME);
+		/*
+		** I chose to propogate any errors back from the shell loop
+		** for debugging and QA
+		*/
 		if (mini_shell(env) == FAILURE)
 			return (mini_error(env, __func__, NULL, "failed"));
 	}
